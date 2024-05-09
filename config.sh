d4085293135e43a2a03fcd26dfd234818dab43d8
Sudipto Bhattacharjee <ttsudipto@gmail.com>
2024-05-09 19:53:02 +0530

    Configuration file containing the paths of the tools

---
+++ config.sh
@@ -0,0 +1,10 @@
+freebayes_path=/usr/bin/freebayes
+samtools_path=/usr/bin/samtools
+bwa_path=/usr/bin/bwa
+trim_galore_path=/usr/bin/trim_galore
+vcflib_path=/usr/bin/vcflib
+bgzip_path=/usr/bin/bgzip
+bcftools_path=/usr/bin/bcftools
+trim_galore_cores=4
+bwa_mem_cores=4
+samtools_cores=4
